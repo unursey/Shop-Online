9ae1a95513442e40895334dceb0eef63398062cc
Anna O <usune@mail.ru>
2022-07-24 04:09:31 +0300

    jest'

--- js/modules/init.js
+++ js/modules/init.js
@@ -1,8 +1,15 @@
 import {searchTimer} from "./searchTimer.js";
+import {createPage, createArticle, } from "./renderBlog.js";
 
 
-export const init = () => {   
-    
-    searchTimer();
+export const init = () => { 
+searchTimer();
+createPage();
+
+
+
+createArticle();
+
+
 }
 
