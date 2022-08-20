ee8857d716310c8b4429d84208ff15f2207bc5a0
Anna O <usune@mail.ru>
2022-08-20 03:28:33 +0300

    sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,13 @@
 import gulp from "gulp";
 import browserSync from "browser-sync";
+import sassPkg from 'sass';
+import gulpSass from 'gulp-sass';
 import cssimport from "gulp-cssimport";
 import del from 'del';
+
+const prepros = true;
+
+const sass = gulpSass(sassPkg);
 
 // задачи
 
@@ -10,17 +16,36 @@ export const html = () => gulp
     .pipe(gulp.dest("dist"))
     .pipe(browserSync.stream());
 
-export const css = () => gulp
-  .src("src/css/index.css")
-  .pipe(cssimport({
-    extensions: ['css'],
-  }))
-  .pipe(gulp.dest("dist/css"))
-  .pipe(browserSync.stream());
+// export const css = () => gulp
+//   .src("src/css/index.css")
+//   .pipe(cssimport({
+//     extensions: ['css'],
+//   }))
+//   .pipe(gulp.dest("dist/css"))
+//   .pipe(browserSync.stream());
+
+export const style = () => {
+  if (prepros) {
+    return gulp
+      .src('src/scss/**/*.scss')
+      .pipe(sass().on('error', sass.logError))  
+      .pipe(gulp.dest('dist/css'))
+      .pipe(browserSync.stream());
+  }
+
+  return gulp
+    .src("src/css/index.css")
+    .pipe(cssimport({
+      extensions: ['css'],
+    }))
+    .pipe(gulp.dest("dist/css"))
+    .pipe(browserSync.stream());
+}
 
 export const js = () => gulp
     .src("src/js/**/*.js")
     .pipe(gulp.dest("dist/js"))
+    .pipe(browserSync.stream())
     .pipe(browserSync.stream());
  
 export const copy = () => gulp
@@ -46,7 +71,8 @@ export const server = () => {
   });
 
   gulp.watch('./src/**/*.html', html);
-  gulp.watch('./src/css/**/*.css', css);
+  // gulp.watch('./src/css/**/*.css', css);
+  gulp.watch(prepros ? './src/scss/**/*.scss' : './src/css/**/*.css', style);
   gulp.watch('./src/js/**/*.js', js);
   gulp.watch(['./src/img/**/*', './src/fonts/**/*'], copy);
 };
@@ -55,7 +81,7 @@ export const clear = () => del('dist/**/*', {forse: true,});
 
 // запуск
 
-export const base = gulp.parallel(html, css, js, copy);
+export const base = gulp.parallel(html, style, js, copy);
 
 export const build = gulp.series(clear, base);
 
